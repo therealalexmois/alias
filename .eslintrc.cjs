9060f24afe359815ab8698afcfece3084bb1ad95
Aleksander Moiseenko <alex.mois.developer@gmail.com>
2022-08-02 19:29:53 +0300

    chore: update settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require("@rushstack/eslint-patch/modern-module-resolution")
 
 module.exports = {
   root: true,
@@ -7,6 +7,37 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier",
+    "plugin:prettier/recommended",
   ],
-};
+  plugins: ["prettier"],
+  rules: {
+    "@typescript-eslint/no-extra-semi": "off",
+    "@typescript-eslint/semi": "off",
+    "no-extra-semi": "off",
+    "no-space-before-semi": "off",
+    semi: "off",
+    "semi-spacing": "off",
+    "semi-style": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        arrowParens: "always",
+        bracketSpacing: true,
+        endOfLine: "lf",
+        htmlWhitespaceSensitivity: "css",
+        insertPragma: false,
+        bracketSameLine: false,
+        jsxSingleQuote: false,
+        printWidth: 80,
+        proseWrap: "always",
+        quoteProps: "as-needed",
+        requirePragma: false,
+        semi: false,
+        singleQuote: false,
+        tabWidth: 2,
+        trailingComma: "all",
+        useTabs: false,
+      },
+    ],
+  },
+}
