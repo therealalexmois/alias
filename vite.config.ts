5a55788b6627155b0fdc504bff0f6785bd854e0a
Aleksander Moiseenko <alex.mois.developer@gmail.com>
2022-08-21 11:01:30 +0300

    chore: update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
 import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import vue from "@vitejs/plugin-vue"
 import vueJsx from "@vitejs/plugin-vue-jsx"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+
+  return {
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+    server: {
+      port: Number(env.PORT),
+    },
+  }
 })
