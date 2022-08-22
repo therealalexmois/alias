0eda9ef1fb70413421cc2dda4328a9c50a37004b
Aleksander Moiseenko <alex.mois.developer@gmail.com>
2022-08-22 09:12:43 +0300

    refactor: add prop class for button outlined

--- src/ui/atoms/button-outlined.vue
+++ src/ui/atoms/button-outlined.vue
@@ -1,28 +1,35 @@
 <script setup lang="ts">
+import { ref, computed } from "vue"
 import classNames from "classNames"
+
+import { mergeClasses } from "@/lib/css"
 
 interface Props {
   as?: string
   to?: string
+  class?: string
 }
 
 const props = withDefaults(defineProps<Props>(), {
   as: "button",
 })
+
+const defaultClasses = ref(
+  classNames(
+    "w-full h-12 inline-flex items-center justify-center",
+    "font-sans text-xl font-semibold leading-6 text-white",
+    "bg-black rounded-[0.652rem] ring-1 ring-white",
+    "tablet:h-14 tablet:text-2xl",
+  ),
+)
+
+const propsClasses = ref(props.class)
+const classes = computed(() =>
+  mergeClasses(defaultClasses.value, propsClasses.value),
+)
 </script>
 <template>
-  <component
-    :is="as"
-    :to="to"
-    :class="
-      classNames(
-        'w-full h-12 inline-flex items-center justify-center',
-        'font-sans text-xl font-semibold leading-6 text-white',
-        'bg-black rounded-[0.652rem] ring-1 ring-white',
-        'tablet:h-14 tablet:text-2xl',
-      )
-    "
-  >
+  <component :is="as" :to="to" :class="classes">
     <slot />
   </component>
 </template>
