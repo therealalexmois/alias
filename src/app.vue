4c29dc6be080fb4a209c95d0b3d54ff1ddf6a89b
Aleksander Moiseenko <alex.mois.developer@gmail.com>
2022-09-04 21:56:07 +0300

    chore: add app.vue

---
+++ src/app.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <router-view v-slot="{ Component, route }">
+      <transition :name="route.meta.transition || 'slide'" mode="out-in">
+        <component :is="Component" :key="$route.path"></component>
+      </transition>
+    </router-view>
+  </div>
+</template>
+<style scoped lang="css">
+.slide-right-enter-active,
+.slide-right-leave-active {
+  transition: opacity 0.5s, transform 0.5s;
+}
+
+.slide-right-enter-from,
+.slide-right-leave-to {
+  opacity: 0;
+  transform: translateX(30%);
+}
+
+.slide-left-enter-active,
+.slide-left-leave-active {
+  transition: opacity 0.5s, transform 0.5s;
+}
+.slide-left-enter-from,
+.slide-left-leave-to {
+  opacity: 0;
+  transform: translateX(-30%);
+}
+</style>
+>
+
